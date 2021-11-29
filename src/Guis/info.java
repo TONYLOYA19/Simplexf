6222609c1d60500ecb66f1ced5322b0730583405
TONYLOYA19 <charly.1904@hotmail.com>
2021-11-29 15:31:32 -0600

    Actualizacion De Proyecto

---
+++ src/Guis/info.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Guis;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author charl
+ */
+class info {
+
+    static void setText(String no_se_ha_podido_encontrar_una_solución_al) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setBackground(Color color) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/proeyectoido/ProeyectoIdO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proeyectoido;
+
+import Guis.GuiInicio;
+
+/**
+ *
+ * @author Mipc
+ */
+public class ProeyectoIdO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      java.awt.EventQueue.invokeLater(() -> {
+          //                new Princial1().setVisible(true);
+//                new Principal2().setVisible(true);
+//new  GuiAsignacion().setVisible(true);
+new GuiInicio().setVisible(true);
+      });
+    }
+    
+}
